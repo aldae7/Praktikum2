5a5600fd3cd391ab97be90ebf0abb1fc48bbbc44
aldae7 <aldinofajrian2@mhs.pelitabangsa.ac.id>
2018-10-15 10:58:52 +0700

    soal1

---
+++ soal1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cout << "masukkan bilangan 1: "; cin >> A;
+    cout << "masukkan bilangan 2: "; cin >> B;
+    cout << "masukkan bilangan 3: "; cin >> C;
+
+    if (A > B) {
+        if (A > C)
+            cout << "Bilangan terbesar adalah: " << A << endl;
+        else
+            cout << "Bilangan terbesar adalah: " << C << endl;
+    } else {
+        if (B > C)
+            cout << "Bilangan terbesar adalaj: " << B << endl;
+        else
+            cout << "Bilangan terbesar adalah: " << C << endl;
+    }
+}
